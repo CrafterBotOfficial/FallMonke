afd7649370ae331ca95b88e3d994e9f885fae87b
Crafterbot <>
2025-08-16 18:18:19 -0400

    testing serializer view

--- Participant.cs
+++ Participant.cs
@@ -1,3 +1,6 @@
 namespace FallMonke;
 
-public class Participant(NetPlayer player, VRRig rig, bool IsAlive);
+public class Participant(NetPlayer player, VRRig rig, bool IsAlive = true) {
+    public bool IsAlive { get; set; } = IsAlive;
+    public ParticipantManager Manager { get; set; } = rig.AddComponent<ParticipantManager>();
+}
