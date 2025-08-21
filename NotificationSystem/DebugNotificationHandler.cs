a7a67460a3ce792e14468f9a6561fb55b583aed6
Chin <86007652+Chin0303@users.noreply.github.com>
2025-08-21 14:19:19 +0200

    Added HUD and fixed assetbundle

--- NotificationSystem/DebugNotificationHandler.cs
+++ NotificationSystem/DebugNotificationHandler.cs
@@ -13,7 +13,10 @@ public class DebugNotificationHandler : INotificationHandler
 
     public void Setup()
     {
+        GameObject.FindObjectsByType<TextMeshPro>(FindObjectsSortMode.None).Where(x => x.gameObject.name == "TextHeader").FirstOrDefault().font = GorillaTagger.Instance.offlineVRRig.playerText1.font;
+
         textObject = GameObject.FindObjectsByType<TextMeshPro>(FindObjectsSortMode.None).Where(x => x.gameObject.name == "DebugText").FirstOrDefault();
+        textObject.font = GorillaTagger.Instance.offlineVRRig.playerText1.font;
         textObject.text = string.Empty;
     }
 
