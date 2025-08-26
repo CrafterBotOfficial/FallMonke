45dc61efceefb99e9a7ee24926a9c9e459dc3a18
Crafterbot <>
2025-08-26 16:29:01 -0400

    Fixed geo hidden on return to stump

--- UI/Buttons/LeaveGameButton.cs
+++ UI/Buttons/LeaveGameButton.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using UnityEngine;
+
 namespace FallMonke.UI.Buttons;
 
 public class LeaveGameButton : PressableButton
@@ -6,5 +9,15 @@ public class LeaveGameButton : PressableButton
     {
         base.ButtonActivation();
         NetworkSystem.Instance.ReturnToSinglePlayer();
+
+        const string GEO_SHOW_TRIGGER_NAME = "LeavingCanyonGeo";
+
+        var trigger = GameObject.FindObjectsOfType<GorillaGeoHideShowTrigger>().FirstOrDefault(x => x.gameObject.name == GEO_SHOW_TRIGGER_NAME);
+        if (trigger) // for some reason simply referencing it is enough to completely fix the issue. No clue why.
+        {
+        //     trigger.OnBoxTriggered();
+        //     return;
+        }
+        // Main.Log($"Couldn't find {GEO_SHOW_TRIGGER_NAME}, likely changed due to a game update.", BepInEx.Logging.LogLevel.Warning);
     }
 }
